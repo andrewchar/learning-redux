b841599ec77bd9265c71bb26319122f339bb9a16
Andrew Charlebois <andrew.charlebois@caseware.com>
2018-08-17 09:32:05 -0400

    learning stuff

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,5 +1,5 @@
 export default function(state = null, action) {
-    switch(action) {
+    switch(action.type) {
         case 'BOOK_SELECTED':
             return action.payload;
     }
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: 'JavaScript: The Good Parts'},
-        { title: 'Harry Potter'},
-        { title: 'the Dark Tower'},
-        { title: 'Eloquent Ruby'}
+        { title: 'JavaScript: The Good Parts', pages: 101},
+        { title: 'Harry Potter', pages: 39},
+        { title: 'the Dark Tower', pages: 85},
+        { title: 'Eloquent Ruby', pages: 1}
     ]
 }
